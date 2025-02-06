056bb72507757b2fd16d7c9e7b9f3e2389ddffe1
Priyank Patel <patel23priyank@gmail.com>
2025-02-06 19:37:38 +0000

    add message function

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-const button = document.querySelector('button');
+const btnConfetti = document.querySelector('.btn-confetti');
+const btnSettings = document.querySelector('.btn-settings');
+const btnOkay = document.querySelector('.btn-okay');
+const message = document.querySelector('.message');
+const settings = document.querySelector('.settings');
 const audio = document.querySelector('audio');
 
-button.addEventListener('click', () => {
+btnConfetti.addEventListener('click', () => {
   confetti();
   audio.pause();
   audio.currentTime = 0;
   audio.play();
+});
+
+btnSettings.addEventListener('click', () => {
+  message.value = '';
+  settings.classList.remove('hide');
+  message.focus();
+});
+
+btnOkay.addEventListener('click', () => {
+  const value = message.value.trim().substring(0, 20);
+  btnConfetti.textContent = value === '' ? 'Confetti 🎊' : value;
+  settings.classList.add('hide');
+});
+
+message.addEventListener('keydown', event => {
+  switch (event.key) {
+    case 'Enter':
+      btnOkay.click();
+      break;
+
+    case 'Escape':
+      settings.classList.add('hide');
+      message.value = '';
+      break;
+  }
 });
